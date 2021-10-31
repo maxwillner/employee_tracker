1537b38c2d608af2ce253de595d648074aefb1f5
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-31 11:58:25 -0400

    created db: initial tables; initial seed data

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO employees (first_name, last_name)
+VALUES
+    ("Julius", "Caesar"),
+    ("Magnus", "Pompey"),
+    ("Marc", "Anthony"),
+    ("Titus", "Pullo"),
+    ("Lucius", "Vorenus"),
+    ("Gaius", "Octavian"),
+    ("Marcus", "Brutus");
+
+INSERT INTO departments (title)
+VALUES
+    ("Executive"),
+    ("Legal"),
+    ("Sales"),
+    ("Human Resources (HR)"),
+    ("Operations");
+
+INSERT INTO roles (title, salary)
+VALUES
+    ("President", "250000.00"),
+    ("Vice-President", "200000.00"),
+    ("Lawyer", "150000.00"),
+    ("Account Director", "100000.00"),
+    ("Account Executive", "90000.00"),
+    ("Resource Officer", "80000.00"),
+    ("Operator", "70000.00");
